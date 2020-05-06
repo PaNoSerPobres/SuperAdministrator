a9104446ebe16e9e2f81a535459ad0534f18c9c7
Cristian Erick Marquez <cristian.marqueza@my.unitec.edu.mx>
2020-05-06 16:21:38 -0500

    Login

--- app/src/main/java/com/example/superadministrator/LoginActivity.java
+++ app/src/main/java/com/example/superadministrator/LoginActivity.java
@@ -1,18 +1,73 @@
 package com.example.superadministrator;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 777;
+    List<AuthUI.IdpConfig> providers;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+        showSignInOptions();
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.fui_ic_microsoft_24dp)
+                        //.setTheme()
+                        .build(), RC_SIGN_IN);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        TextView emailTextView = findViewById(R.id.emailTextView);
+        TextView uidTextView = findViewById(R.id.uidTextView);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                goMainScreen();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(this, R.string.error_login, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void goMainScreen() {
@@ -21,17 +76,5 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //firebaseAuth.addAuthStateListener((firebaseAuthListener));
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        //firebaseAuth.removeAuthStateListener(firebaseAuthListener);
-    }
 
 }
--- app/src/main/java/com/example/superadministrator/MainActivity.java
+++ app/src/main/java/com/example/superadministrator/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.login.LoginManager;
+
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -19,14 +19,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 777;
     List<AuthUI.IdpConfig> providers;
-    FirebaseUser user;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
 
     @Override
@@ -34,62 +33,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Providers
-        providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build()
-        );
-
         if(user==null){
-            showSignInOptions();
+            goLoginScreen();
+        }
+        else
+        {
+            TextView nameTextView = findViewById(R.id.nameTextView);
+            TextView emailTextView = findViewById(R.id.emailTextView);
+            TextView uidTextView = findViewById(R.id.uidTextView);
+
+            String name = user.getDisplayName();
+            String email =user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+            String uid = user.getUid();
+
+            nameTextView.setText(name);
+            emailTextView.setText(email);
+            uidTextView.setText(uid);
         }
     }
-
-    private void showSignInOptions() {
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .setLogo(R.drawable.fui_ic_microsoft_24dp)
-                        //.setTheme()
-                        .build(), RC_SIGN_IN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        TextView nameTextView = findViewById(R.id.nameTextView);
-        TextView emailTextView = findViewById(R.id.emailTextView);
-        TextView uidTextView = findViewById(R.id.uidTextView);
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                user = FirebaseAuth.getInstance().getCurrentUser();
-                if(user !=null){
-                    String name = user.getDisplayName();
-                    String email =user.getEmail();
-                    Uri photoUrl = user.getPhotoUrl();
-                    String uid = user.getUid();
-
-                    nameTextView.setText(name);
-                    emailTextView.setText(email);
-                    uidTextView.setText(uid);
-                }
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-                Toast.makeText(this, R.string.error_login, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
 
     private void goLoginScreen() {
         Intent intent = new Intent(this, LoginActivity.class);
@@ -102,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 .signOut(this)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     public void onComplete(@NonNull Task<Void> task) {
-                       showSignInOptions();
+                       goLoginScreen();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                                             @Override
@@ -110,7 +72,5 @@ public class MainActivity extends AppCompatActivity {
                                                 Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
                                             }
                                         });
-
-                //goLoginScreen();
     }
 }
